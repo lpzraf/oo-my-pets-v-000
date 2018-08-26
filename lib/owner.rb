3b94fceb8eabc15c81a6d5cdf82c59d84271c3b3
Rafael Lopez <lpz.raf@gmail.com>
2018-08-26 22:43:23 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -23,6 +23,21 @@ class Owner
   def self.reset_all
     self.all.clear
   end
+  
+  def buy_fish(name)
+    fish = Fish.new(name)
+    self.pets[:fishes] << fish
+  end
+  
+  def buy_cat(name)
+    cat = Cat.new(name)
+    self.pets[:cats] << cat
+  end
+  
+  def buy_dog(name)
+    dog = Dog.new(name)
+    self.pets[:dogs] << dog
+  end
 end
 
 
