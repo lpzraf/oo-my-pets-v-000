db010afc912f3e1578b716ca683e1a527d1c5fc4
Rafael Lopez <lpz.raf@gmail.com>
2018-08-26 21:10:49 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -6,5 +6,18 @@ class Owner
   def initialize(name)
     @name = name
     @@all << self
+  
+  end
+  
+  def self.count
+    @@all.length
+  end
+     
+   def self.all
+     @@all
+   end
+   
+  def self.reset_all
+    self.all.clear
   end
 end
