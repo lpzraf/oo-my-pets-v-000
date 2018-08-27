de35f807bb1ad65d45005517056d00abd0e43352
Rafael Lopez <lpz.raf@gmail.com>
2018-08-27 23:35:51 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,9 +66,7 @@ class Owner
  end
  
   def list_pets
-    self.pets.each do |owner, pet|
-    puts "I have #{owner.length} fish, #{owner.length} dog(s), and #{owner.length} cat(s)."
-  end
+    puts "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
  end
 end
 
